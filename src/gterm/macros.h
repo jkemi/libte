fbdafedb1cd23f9fe93e379e53817636d47898cb
Jakob Kemi <jakob.kemi@gmail.com>
2008-08-17 21:27:45 +0200

    added macros.h

---
+++ src/gterm/macros.h
@@ -0,0 +1,30 @@
+/*
+ * macros.h
+ *
+ *  Created on: Aug 17, 2008
+ *      Author: jakob
+ */
+
+#ifndef MACROS_H_
+#define MACROS_H_
+
+#ifdef _MSC_VER
+  #ifdef BUILDING_DLL
+    #define DLLEXPORT __declspec(dllexport)
+  #else
+    #define DLLEXPORT __declspec(dllimport)
+  #endif
+  #define DLLLOCAL
+#else
+  #ifdef HAVE_GCCVISIBILITYPATCH
+    #define DLLEXPORT __attribute__ ((visibility("default")))
+    #define DLLLOCAL __attribute__ ((visibility("hidden")))
+  #else
+    #define DLLEXPORT
+    #define DLLLOCAL
+  #endif
+#endif
+
+
+
+#endif /* MACROS_H_ */
