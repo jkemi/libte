86c60028380e7aacd54666b1fe51ee2d99400b1e
Jakob Kemi <jakob.kemi@gmail.com>
2008-09-18 23:23:31 +0200

    add errorf, xmalloc_new and friends

---
+++ src/libte/misc.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "misc.h"
+
+
+void errorf(const char* format, ...) {
+	va_list ap;
+	va_start(ap,format);
+	vfprintf(stderr, format, ap);
+	va_end(ap);
+	exit(EXIT_FAILURE);
+}
+
+void _debugf (const char* label, const char* file, const char* func, int line, const char* format, ...)
+{
+	fprintf (stderr, "%s %s (%d) : %s: ", label, file, line, func);
+	va_list va;
+	va_start (va, format);
+	vfprintf (stderr, format, va);
+	va_end (va);
+	fflush(stderr);
+}
+
+void* _xmalloc(const char* func, size_t size) {
+	void* p = malloc(size);
+	if (p == NULL) {
+		errorf("ERROR: %s: xmalloc(%lu) failed with reason: %s\n", func, size, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
+
+void* _xcalloc(const char* func, size_t size) {
+	void* p = calloc(1,size);
+	if (p == NULL) {
+		errorf("ERROR: %s: xcmalloc(%lu) failed with reason: %s\n", func, size, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
+
+void* _xrealloc(const char* func, void* oldptr, size_t size) {
+	void* p = realloc(oldptr,size);
+	if (p == NULL) {
+		errorf("ERROR: %s: xrealloc(%lu) failed with reason: %s\n", func, size, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
--- src/libte/misc.h
+++ src/libte/misc.h
@@ -9,6 +9,7 @@
 #define MISC_H_
 
 #include <stdint.h>
+#include <stddef.h>
 
 typedef unsigned int uint;
 
@@ -37,6 +38,31 @@ static inline uint uint_clamp(uint x, uint lo, uint hi) {
 	}
 	return x;
 }
+
+//
+// Logging
+//
+void errorf(const char* format, ...) __attribute__ ((__format__ (__printf__, 1, 2)));
+
+#define DEBUGF(fmt,...) _debugf("DEBUG:   ",__FILE__,__func__,__LINE__,fmt, ##__VA_ARGS__)
+#define WARNF(fmt,...)  _debugf("WARNING: ",__FILE__,__func__,__LINE__,fmt, ##__VA_ARGS__)
+#define ERRORF(fmt,...) _debugf("ERROR:   ",__FILE__,__func__,__LINE__,fmt, ##__VA_ARGS__)
+// error reporting as both window message and debugger string
+void _debugf(const char* label, const char* file, const char* func, int line, const char* format, ...) __attribute__ ((format (printf, 5, 6)));
+
+//
+// Memory allocation/deallocation
+//
+void* _xmalloc(const char* func, size_t size);
+#define xmalloc(s)				_xmalloc(__func__,s)
+#define xnew(t,n)				(t*)xmalloc(sizeof(t)*(n))
+#define xnew_aligned(t,a,n)		(t*)xmalloc_aligned(a,sizeof(t)*(n))
+
+void* _xcalloc(const char* func, size_t size);
+#define xcalloc(s)				_xcalloc(__func__,s)
+#define xcnew(t,n)				(t*)xcalloc(sizeof(t)*(n))
+
+#define allocanew(t,n)				(t*)alloca(sizeof(t)*(n))
 
 
 
