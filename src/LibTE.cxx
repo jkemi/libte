590236c864597b39f6e9667b4e6b4ebf1e6e7f5b
Jakob Kemi <jakob.kemi@gmail.com>
2010-02-17 19:04:04 +0100

    cleanups in c++ wrapper

--- src/LibTE.cxx
+++ src/LibTE.cxx
@@ -8,65 +8,22 @@
 #include "LibTE.hpp"
 
 
-static void _impl_draw_text (void* priv, int x, int y, const symbol_t* symbols, int len) {
-	((TE*)priv)->fe_draw_text(x, y, symbols, len);
-}
-static void _impl_draw_clear (void* priv, int x, int y, const symbol_color_t bg_color, int len) {
-	((TE*)priv)->fe_draw_clear(x, y, bg_color, len);
-}
-static void _impl_draw_cursor (void* priv, symbol_color_t fg_color, symbol_color_t bg_color, symbol_attributes_t attrs, int x, int y, int32_t cp) {
-	((TE*)priv)->fe_draw_cursor(fg_color, bg_color, attrs, x, y, cp);
-}
-static void _impl_draw_move (void* priv, int y, int height, int byoffset) {
-	((TE*)priv)->fe_draw_move(y, height, byoffset);
-}
-static void _impl_updated (void* priv) {
-	((TE*)priv)->fe_updated();
-}
-static void _impl_reset (void* priv) {
-	((TE*)priv)->fe_reset();
-}
-static void _impl_bell (void* priv) {
-	((TE*)priv)->fe_bell();
-}
-static void _impl_title (void* priv, const int32_t* text) {
-	((TE*)priv)->fe_title(text);
-}
-static void _impl_send_back (void* priv, const int32_t* data, int len) {
-	((TE*)priv)->fe_send_back(data, len);
-}
-static void _impl_request_resize (void* priv, int width, int height) {
-	((TE*)priv)->fe_request_resize(width, height);
-}
-static void _impl_position (void* priv, int offset, int size) {
-	((TE*)priv)->fe_position(offset, size);
-}
-
-
-const static TE_Frontend _callbacks = {
-		&_impl_draw_text,
-		&_impl_draw_clear,
-		&_impl_draw_cursor,
-		&_impl_draw_move,
-
-		&_impl_updated,
-		&_impl_reset,
-		&_impl_bell,
-		&_impl_title,
-		&_impl_send_back,
-		&_impl_request_resize,
-		&_impl_position,
-};
-
-
-
 void TE::teInit(int width, int height) {
+	const static TE_Frontend _callbacks = {
+			&_impl_draw_text,
+			&_impl_draw_clear,
+			&_impl_draw_cursor,
+			&_impl_draw_move,
+
+			&_impl_updated,
+			&_impl_reset,
+			&_impl_bell,
+			&_impl_title,
+			&_impl_send_back,
+			&_impl_request_resize,
+			&_impl_position,
+	};
+
 	_te = te_create(&_callbacks, this, width, height);
 }
 
-
-TE::~TE() {
-	if (_te != NULL) {
-		te_destroy(_te);
-	}
-}
--- src/LibTE.hpp
+++ src/LibTE.hpp
@@ -4,9 +4,15 @@ class TE {
 private:
 	TE_Backend*	_te;
 public:
+
 	TE() {_te = NULL;}
 	TE(const TE&);	// Intentionally not defined.
-	virtual ~TE();
+	virtual ~TE() {
+		if (_te != NULL) {
+			te_destroy(_te);
+		}
+	}
+
 
 	void teInit(int width, int height);
 
@@ -61,33 +67,48 @@ public:
 		te_handle_keypress(_te, cp, modifiers);
 	}
 
+	/**
+	 * Handles mouse input
+	 */
 	void teHandleMouse(int mouse_x, int mouse_y, te_mouse_button_t mouse_buttons, te_modifier_t modifiers) {
 		te_handle_mouse(_te, mouse_x, mouse_y, mouse_buttons, modifiers);
 	}
 
+	/**
+	 * Handles pasting of text
+	 */
 	void tePasteText(const int32_t* data, size_t len) {
 		te_paste_text(_te, data, len);
 	}
 
+	/**
+	 * Handles setting of scroll-back position
+	 */
 	void tePosition(int offset) {
 		te_position(_te, offset);
 	}
 
+	/**
+	 * Handles setting of scroll-lock
+	 */
 	void teLockScroll(bool scroll_lock) {
 		te_lock_scroll(_te, scroll_lock);
 	}
 
 #ifndef NDEBUG
+	/**
+	 * Dumps internal debug output from terminal implementation.
+	 */
 	void teDebug(FILE* where) {
 		te_debug(_te, where);
 	}
 #endif
 
+protected:
 	virtual void fe_draw_text(int x, int y, const symbol_t* symbols, int len) = 0;
 	virtual void fe_draw_clear(int x, int y, const symbol_color_t bg_color, int len) = 0;
 	virtual void fe_draw_cursor(symbol_color_t fg_color, symbol_color_t bg_color, symbol_attributes_t attrs, int x, int y, int32_t cp) = 0;
 	virtual void fe_draw_move(int y, int height, int byoffset) = 0;
-
 	virtual void fe_updated() = 0;
 	virtual void fe_reset() = 0;
 	virtual void fe_bell() = 0;
@@ -95,4 +116,40 @@ public:
 	virtual void fe_send_back(const int32_t* data, int len) = 0;
 	virtual void fe_request_resize(int width, int height) = 0;
 	virtual void fe_position(int offset, int size) = 0;
+
+private:
+	static void _impl_draw_text (void* priv, int x, int y, const symbol_t* symbols, int len) {
+		((TE*)priv)->fe_draw_text(x, y, symbols, len);
+	}
+	static void _impl_draw_clear (void* priv, int x, int y, const symbol_color_t bg_color, int len) {
+		((TE*)priv)->fe_draw_clear(x, y, bg_color, len);
+	}
+	static void _impl_draw_cursor (void* priv, symbol_color_t fg_color, symbol_color_t bg_color, symbol_attributes_t attrs, int x, int y, int32_t cp) {
+		((TE*)priv)->fe_draw_cursor(fg_color, bg_color, attrs, x, y, cp);
+	}
+	static void _impl_draw_move (void* priv, int y, int height, int byoffset) {
+		((TE*)priv)->fe_draw_move(y, height, byoffset);
+	}
+	static void _impl_updated (void* priv) {
+		((TE*)priv)->fe_updated();
+	}
+	static void _impl_reset (void* priv) {
+		((TE*)priv)->fe_reset();
+	}
+	static void _impl_bell (void* priv) {
+		((TE*)priv)->fe_bell();
+	}
+	static void _impl_title (void* priv, const int32_t* text) {
+		((TE*)priv)->fe_title(text);
+	}
+	static void _impl_send_back (void* priv, const int32_t* data, int len) {
+		((TE*)priv)->fe_send_back(data, len);
+	}
+	static void _impl_request_resize (void* priv, int width, int height) {
+		((TE*)priv)->fe_request_resize(width, height);
+	}
+	static void _impl_position (void* priv, int offset, int size) {
+		((TE*)priv)->fe_position(offset, size);
+	}
+
 };
